95e6bc50498d85ae3e78b02bea6b718220781d85
gaur-j <gjo125804@gmail.com>
2024-09-18 19:23:38 +0530

    first project

---
+++ app.js
@@ -0,0 +1,88 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-game");
+let newBtm = document.querySelector("#new-game");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turn0 = true;
+let count = 0;
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+const resetGame = () => {
+  turn0 = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turn0) {
+      box.innerHTML = "0";
+      turn0 = false;
+    } else {
+      box.innerHTML = "x";
+      turn0 = true;
+    }
+
+    box.disabled = true;
+    count++;
+
+    let isWinner = checkWinner();
+    if (count === 9 && !isWinner) {
+      gameDraw();
+    }
+  });
+});
+
+const disabledBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.enabled = true;
+    box.innerHTML = "";
+  }
+};
+const showWinner = (winner) => {
+  msg.innerHTML = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disabledBoxes();
+};
+
+const gameDraw = () => {
+  msg.innerHTML = `Oh No Game Draw.`;
+  msgContainer.classList.remove("hide");
+  disabledBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerHTML;
+    let pos2Val = boxes[pattern[1]].innerHTML;
+    let pos3Val = boxes[pattern[2]].innerHTML;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+        return true;
+      }
+    }
+  }
+};
+
+resetBtn.addEventListener("click", resetGame);
+newBtm.addEventListener("click", resetGame);
